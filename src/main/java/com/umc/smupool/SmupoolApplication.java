e6c048831233dc84fdc71b7552ea86083442c87c
jinho7 <69239293+jinho7@users.noreply.github.com>
2024-05-21 04:03:43 +0900

    first commit

---
+++ src/main/java/com/umc/smupool/SmupoolApplication.java
@@ -0,0 +1,13 @@
+package com.umc.smupool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmupoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmupoolApplication.class, args);
+	}
+
+}
