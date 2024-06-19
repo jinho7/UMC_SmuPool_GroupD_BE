9df7193b4763b091b40a3502e8fb04fe236cc896
gomin0 <gomin0981113@naver.com>
2024-06-19 21:53:30 +0900

    🌏 ci: ci/cd 파이프라인 구축 (#34)

---
+++ src/main/java/com/umc/smupool/domain/map/entity/Matching.java
@@ -0,0 +1,44 @@
+package com.umc.smupool.domain.map.entity;
+
+import com.umc.smupool.domain.map.entity.enums.Status;
+import com.umc.smupool.domain.member.entity.Member;
+import com.umc.smupool.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@DynamicUpdate
+@DynamicInsert
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Matching extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="matching_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "DEFAULT 'PENDING'")
+    private Status status;
+
+    private LocalDateTime time;
+
+    private int goal_num;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "carpool_zone_id")
+    private CarpoolZone carpoolZone;
+
+    @OneToMany(mappedBy = "matching", cascade = CascadeType.ALL)
+    private List<Member> memberList = new ArrayList<>();
+
+}
---
+++ src/main/java/com/umc/smupool/domain/member/service/MemberService.java
@@ -0,0 +1,17 @@
+package com.umc.smupool.domain.member.service;
+
+import com.umc.smupool.domain.member.dto.MemberRequestDTO;
+import com.umc.smupool.domain.member.entity.Member;
+
+import java.util.List;
+
+public interface MemberService {
+    Member join(MemberRequestDTO.JoinDTO joinDTO);
+
+    Member readMember(Long memberId);
+    List<Member> readMembers();
+
+    void deleteMember(Long memberId);
+
+    Member updateMember(MemberRequestDTO.UpdateMemberDTO updateMemberDTO, Long memberId);
+}
