7ac9251e0e4a47cf9dae7062f388186f496a4ead
jinnieusLab <kitty14904@naver.com>
2024-06-20 20:06:37 +0900

    :sparkles:  feat: JWT 발급

---
+++ src/main/java/com/umc/smupool/domain/member/exception/handler/AuthHandler.java
@@ -0,0 +1,11 @@
+package com.umc.smupool.domain.member.exception.handler;
+
+import com.umc.smupool.domain.member.entity.Member;
+import com.umc.smupool.global.apiPayload.code.BaseErrorCode;
+import com.umc.smupool.global.apiPayload.exception.GeneralException;
+
+public class AuthHandler extends GeneralException {
+    public AuthHandler(BaseErrorCode errorCode) {
+        super(errorCode);
+    }
+}
--- src/main/java/com/umc/smupool/global/config/SecurityConfig.java
+++ src/main/java/com/umc/smupool/global/config/SecurityConfig.java
@@ -1,26 +1,35 @@
 package com.umc.smupool.global.config;
 
+import com.umc.smupool.global.config.filter.LoginFilter;
+import com.umc.smupool.global.config.jwt.JWTUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JWTUtil jwtUtil;
     private final String[] allowUrl = {
             "/swagger-ui/**",
             "/swagger-resources/**",
-            "/v3/api-docs/**"
+            "/v3/api-docs/**",
+            "/login"
     };
 
     @Bean
@@ -31,14 +40,25 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
+
         http.authorizeHttpRequests((requests) -> requests
                 .requestMatchers(HttpMethod.POST,"/members/").permitAll()
                 .requestMatchers(HttpMethod.GET,"/members/{memberId}").hasRole("USER")
                 .requestMatchers(HttpMethod.GET,"/members/").hasRole("USER")
                 .requestMatchers(allowUrl).permitAll()
                 .anyRequest().authenticated());
-        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
+
+        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration),jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+        http.formLogin(AbstractHttpConfigurer::disable);
+        http.httpBasic(AbstractHttpConfigurer::disable);
+
+        http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
