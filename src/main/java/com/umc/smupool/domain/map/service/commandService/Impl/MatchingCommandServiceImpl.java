e9cdd2a43786c3d06c7efc8bd0eada15777be7fd
gomin0 <gomin0981113@naver.com>
2024-06-23 17:44:55 +0900

    :recycle: refactor: 매칭 서비스 수정 (#60)

--- src/main/java/com/umc/smupool/domain/map/service/commandService/Impl/MatchingCommandServiceImpl.java
+++ src/main/java/com/umc/smupool/domain/map/service/commandService/Impl/MatchingCommandServiceImpl.java
@@ -23,7 +23,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
-import java.lang.reflect.MalformedParameterizedTypeException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
